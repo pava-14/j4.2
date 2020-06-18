29f119cf91ce0b3967e8f4b09fed72dbb17055cd
Alexander Pavlenko <pava@bk.ru>
2020-06-18 12:22:26 +0700

    Repository Metods Added

--- src/main/java/ru/netology/domain/Offer.java
+++ src/main/java/ru/netology/domain/Offer.java
@@ -1,4 +1,13 @@
 package ru.netology.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Offer {
+    private int id;
+    private String flightFrom;
+    private String flightTo;
+    private int flightTime;
 }
